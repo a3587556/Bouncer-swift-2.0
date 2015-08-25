b574de690a3605a752da34270d59d5add7f4322a
mac <mac@macdeMacBook-Pro.local>
2015-08-25 15:24:00 +0800

    Bouncer swift 2.0

---
+++ Bouncer/BouncerBehavior.swift
@@ -0,0 +1,55 @@
+//
+//  BouncerBehavior.swift
+//  Bouncer
+//
+//  Created by mac on 15/8/25.
+//  Copyright © 2015年 mac. All rights reserved.
+//
+
+import UIKit
+
+class BouncerBehavior: UIDynamicBehavior {
+    let gravity = UIGravityBehavior()
+    
+    lazy var collider: UICollisionBehavior = {
+        let lazilyCreatedDynamicAnimator = UICollisionBehavior()
+        lazilyCreatedDynamicAnimator.translatesReferenceBoundsIntoBoundary = true
+        return lazilyCreatedDynamicAnimator
+        }()
+    
+    lazy var blockBehavior: UIDynamicItemBehavior = {
+        let lazilyCreatedBlockBehavior = UIDynamicItemBehavior()
+        lazilyCreatedBlockBehavior.allowsRotation = true
+        lazilyCreatedBlockBehavior.elasticity = 0.85
+        lazilyCreatedBlockBehavior.friction = 0
+        lazilyCreatedBlockBehavior.resistance = 0
+        return lazilyCreatedBlockBehavior
+        }()
+    
+    override init() {
+        super.init()
+        addChildBehavior(gravity)
+        addChildBehavior(collider)
+        addChildBehavior(blockBehavior)
+    }
+    
+    func addBarrier(path: UIBezierPath, named name: String) {
+        collider.removeBoundaryWithIdentifier(name)
+        collider.addBoundaryWithIdentifier(name, forPath: path)
+    }
+    
+    func addBlock(block: UIView) {
+        dynamicAnimator?.referenceView?.addSubview(block)
+        gravity.addItem(block)
+        collider.addItem(block)
+        blockBehavior.addItem(block)
+    }
+    
+    func removeBlock(block: UIView) {
+        gravity.removeItem(block)
+        collider.removeItem(block)
+        blockBehavior.removeItem(block)
+        block.removeFromSuperview()
+    }
+
+}
